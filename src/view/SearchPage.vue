86a13bb7887e895196885c6fb1b1901818cee307
ltphat2204 <ltphat2204@gmail.com>
2024-05-29 23:47:40 +0700

    Add search input

--- src/view/SearchPage.vue
+++ src/view/SearchPage.vue
@@ -1,3 +1,18 @@
+<script setup>
+import { ref } from 'vue';
+const searchTerm = ref('');
+const search = () => {
+    console.log(searchTerm.value);
+};
+</script>
+
 <template>
-    <div></div>
+    <div>
+        <input
+            v-model="searchTerm"
+            @input="search"
+            type="text"
+            class="w-full p-2 border border-gray-300 rounded-md"
+            placeholder="Search for a meal..." />
+    </div>
 </template>
